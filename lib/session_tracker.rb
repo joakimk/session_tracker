87d2ca4bb99a701aa53f4858d2b7aac2bca808a3
Andrew Cantino <pair+andrew@mavenlink.com>
2012-05-03 21:08:31 -0700

    use REDIS or $redis; accept options, including :propagate_exceptions

--- lib/session_tracker.rb
+++ lib/session_tracker.rb
@@ -3,23 +3,27 @@ require 'time'
 class SessionTracker
   ONE_HOUR = 60 * 60
 
-  def initialize(type, redis = $redis)
+  attr_accessor :options, :redis, :type
+
+  def initialize(type, redis = nil, options = {})
     @type = type
-    @redis = redis
+    @redis = redis || (defined?($redis) && $redis) || (defined?(REDIS) && REDIS)
+    @options = {}
   end
 
   def track(id, time = Time.now)
     return unless id
     key = key_for(time)
-    @redis.sadd(key, id)
-    @redis.expire(key, ONE_HOUR - 60)
+    redis.sadd(key, id)
+    redis.expire(key, ONE_HOUR - 60)
   rescue
     # This is called for every request and is probably not essential for the app
     # so we don't want to raise errors just because redis is down for a few seconds.
+    raise if options[:propagate_exceptions]
   end
 
   def active_users_data(timespan_in_minutes, time)
-    @redis.sunion(*keys_within(timespan_in_minutes, time))
+    redis.sunion(*keys_within(timespan_in_minutes, time))
   end
 
   def active_users(timespan_in_minutes = 5, time = Time.now)
@@ -27,13 +31,13 @@ class SessionTracker
   end
 
   private
-  
+
   def keys_within(minutes, time)
     times = 0.upto(minutes - 1).map { |n| time - (n * 60) }
     times.map { |t| key_for(t) }
   end
 
   def key_for(time)
-    "active_#{@type}_sessions_minute_#{time.strftime("%M")}"
+    "active_#{type}_sessions_minute_#{time.strftime("%M")}"
   end
 end
